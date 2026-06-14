d246e51d560cdeefdc6556dd26ba8251dc36b36c
vennela_p <vennelaprattipati2006@gmail.com>
2026-06-14 12:21:25 +0530

    Add files via upload

---
+++ addition.py
@@ -0,0 +1,4 @@
+a = int(input("enter first number="))
+b = int(input("enter secound number="))
+c = int(input("enter third number="))
+print("add:",a+b+c)
---
+++ variables.py
@@ -0,0 +1,25 @@
+name="vennela"
+age=20
+print(name)
+print(age)
+
+name = input("enter your name:")
+print(name)
+
+name = input("enter your name: ")
+print("hello", name)
+
+city = input("enter your city: ")
+print("city:",city)
+
+age = int(input("enter your age:"))
+print(age)
+
+a = int(input("enter first number="))
+b = int(input("enter secound number="))
+print("addition:",a+b)
+
+a = int(input("enter first number="))
+b = int(input("enter secound number="))
+c = int(input("enter third number="))
+print("add:",a+b+c)
